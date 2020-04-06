b6532ba26fe363c9cf47e850297370273361cb84
EvgHrn <evg.hrn@gmail.com>
2020-04-06 14:23:52 +0400

    Parse all

--- src/utils/Parser.js
+++ src/utils/Parser.js
@@ -42,10 +42,102 @@ export class Parser {
 		return str.match(footerRegExp);
 	};
 
+	static getImages = (body) => {
+		return body.trim().split("\n\r");
+	};
+
+	static headParse = (headStr) => {
+
+		const startDate = headStr.match(/(?<=StartDate)\s+.+/)[0].trim();
+		const startTime = headStr.match(/(?<=StartTime)\s+.+/)[0].trim();
+		const printer = headStr.match(/(?<=Printer)\s+.+/)[0].trim();
+		const halftone = headStr.match(/(?<=Halftone)\s+.+/)[0].trim();
+		const copies = headStr.match(/(?<=Copies)\s+.+/)[0].trim();
+		const mirror = headStr.match(/(?<=Mirror)\s+.+/)[0].trim();
+		const density = headStr.match(/(?<=Density)\s+.+/)[0].trim();
+		const printerProfile = headStr.match(/(?<=PrinterProfile)\s+.+/)[0].trim();
+
+		return {
+			startDate,
+			startTime,
+			printer,
+			halftone,
+			copies,
+			mirror,
+			density,
+			printerProfile
+		};
+	};
+
+	/**
+	 *
+	 * @param imageStr
+	 * @returns {{imgName: string, inputProfile: string, widthCm: string, heightCm: string, rotated: number, areaCm2: string}}
+	 */
+	static imageParse = (imageStr) => {
+
+		const imgName = imageStr.match(/(?<=ImgName)\s+.+/)[0].trim();
+		const widthCm = imageStr.match(/(?<=WidthCm)\s+.+/)[0].trim();
+		const heightCm = imageStr.match(/(?<=HeightCm)\s+.+/)[0].trim();
+		const areaCm2 = imageStr.match(/(?<=AreaCm2)\s+.+/)[0].trim();
+		const inputProfile = imageStr.match(/(?<=InputProfile)\s+.+/)[0].trim();
+		let rotated = 0;
+
+		try {
+			rotated = imageStr.match(/(?<=Rotated)\s+.+/)[0].trim();
+		} catch (e) {
+			// rotated = 0;
+		}
+
+		return {
+			imgName,
+			widthCm,
+			heightCm,
+			areaCm2,
+			inputProfile,
+			rotated
+		};
+
+	};
+
+	static footerParse = (footerStr) => {
+
+		const consumedCm = footerStr.match(/(?<=ConsumedCm)\s+.+/)[0].trim();
+		const inkCyan = footerStr.match(/(?<=InkCyan)\s+.+/)[0].trim();
+		const inkMagenta = footerStr.match(/(?<=InkMagenta)\s+.+/)[0].trim();
+		const inkYellow = footerStr.match(/(?<=InkYellow)\s+.+/)[0].trim();
+		const inkBlack = footerStr.match(/(?<=InkBlack)\s+.+/)[0].trim();
+		const inkLC = footerStr.match(/(?<=InkLC)\s+.+/)[0].trim();
+		const inkLM = footerStr.match(/(?<=InkLM)\s+.+/)[0].trim();
+		const printEnd = footerStr.match(/(?<=PrintEnd)\s+.+/)[0].trim();
+		const minutesTotal = footerStr.match(/(?<=MinutesTotal)\s+.+/)[0].trim();
+
+		let aborted = "0";
+
+		try {
+			aborted = footerStr.match(/(?<=Aborted)\s+.+/)[0].trim();
+		} catch (e) {
+			;
+		}
+
+		return {
+			consumedCm,
+			inkCyan,
+			inkMagenta,
+			inkYellow,
+			inkBlack,
+			inkLC,
+			inkLM,
+			printEnd,
+			minutesTotal,
+			aborted
+		};
+	};
+
 	static parse = (str) => {
 
 		const heads = this.getHeads(str);
-		console.log('jobs: ', heads);
+		console.log('heads: ', heads);
 
 		const footers = this.getFooters(str);
 		console.log('footers: ', footers);
@@ -56,5 +148,25 @@ export class Parser {
 
 		const bodies = this.getBodies(str, heads, footers);
 		console.log('bodies: ', bodies);
+
+		let result = [];
+
+		for (let i = 0; i < heads.length; i++) {
+
+			const imagesStrArr = this.getImages(bodies[i]);
+
+			const imageObjectsArr = imagesStrArr.map((imageStr) => this.imageParse(imageStr));
+
+			result.push({
+				...this.headParse(heads[i]),
+				...this.footerParse(footers[i]),
+				images: imageObjectsArr
+			});
+
+		}
+
+		console.log("result: ", result);
+
+		return result;
 	}
 }
